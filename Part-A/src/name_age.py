813f43df59fe67b9cb685eba9de756af6fcc5d6d
jaydog0911 <jason.dean4@snhu.edu>
2026-09-15 11:55:12 -0400

    Fix age input prompt

--- Part-A/src/name_age.py
+++ Part-A/src/name_age.py
@@ -30,7 +30,7 @@ def main() -> None:
 
     # Get user input.
     name = input("What is your name? ")
-    age = int(input("What is your age? "))
+    age = int(input("How old are you? "))
 
     # Calculate user's approximate birth year.
     birth_year = CURRENT_YEAR - age
